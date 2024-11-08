c564bce62f4f7222a70c8b177b12061a2d020bce
CyanineOrZ <hyx447389036@126.com>
2024-11-08 10:56:13 +0800

    购买部分更新，添加部分图片资源

--- src/Views/Buy.jsx
+++ src/Views/Buy.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
 import '../assets/css/Buy.scss'
+import Steam from "../assets/img/body/buy/buy_store_button_steam.png";
+import Sekai from "../assets/img/body/buy/buy_store_button_sekai.png";
+import Denpa from "../assets/img/body/buy/buy_store_button_denpa.png";
 
 export default class Buy extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        linkList: [
+            { png: Denpa, link: 'https://denpasoft.com/products/mahou-arms/' },
+            { png: Steam, link: 'https://store.steampowered.com/app/1165870/Mahou_Arms' },
+            { png: Sekai, link: 'https://sekaiproject.com/products/mahou-arms/' },
+        ]
+    }
+
     render() {
         return (
-            <>git
+            <>
                 <div className={'buy-contain'}>
                     <div className={'buy-square-contain'}>
-
+                        {this.state.linkList.map((item) => {
+                            return (
+                                <BuySquare png={item.png} link={item.link}></BuySquare>
+                            )
+                        })}
                     </div>
                 </div>
             </>
@@ -17,10 +36,21 @@ export default class Buy extends React.Component {
 }
 
 class BuySquare extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+
+    }
+
     render() {
         return (
             <div className={'square-item'}>
-
+                <a className={'link'} href={this.props.link}>
+                    <img src={this.props.png} alt={''} className={'image'}/>
+                </a>
             </div>
         )
     }
